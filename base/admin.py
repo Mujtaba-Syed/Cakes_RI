8aa09dc82970dbba42475d91e47bda71ceca984a
Mujtaba-Syed <mujtaba3198@gmail.com>
2025-09-08 17:15:21 +0500

    fix error

--- base/admin.py
+++ base/admin.py
@@ -3,7 +3,7 @@ from .models import *
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category', 'is_active','updated_at','created_at')
+    list_display = ('id', 'name', 'price', 'category', 'is_active','updated_at')
     list_filter = ('category', 'is_active')
 admin.site.register(UserProfile)
 # admin.site.register(Customer)
